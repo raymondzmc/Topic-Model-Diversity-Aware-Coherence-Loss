10a1bf80d68e6299d99d4368a630b3a48bcf8178
Raymond Li <li.raymond96@gmail.com>
2022-12-25 10:26:47 -0800

    added evaluation

--- run_topic_models.py
+++ run_topic_models.py
@@ -1,14 +1,24 @@
 import os
+import random
 import argparse
 
+import torch
+import numpy as np
 
 from contextualized_topic_models.models.ctm import CombinedTM, ZeroShotTM
 from contextualized_topic_models.utils.data_preparation import TopicModelDataPreparation
 from contextualized_topic_models.utils.data_preparation import bert_embeddings_from_file
+from contextualized_topic_models.evaluation.measures import TopicDiversity, CoherenceNPMI, CoherenceCV, CoherenceWordEmbeddings, InvertedRBO
+
+import pdb
 
 # Disable tokenizer warning
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+def set_random_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(0)
 
 def load_dataset(text_file, bow_file):
     input_text = []
@@ -31,6 +41,23 @@ def load_dataset(text_file, bow_file):
     return input_text, input_bow
 
 
+def evaluate(topics, text_for_bow, embeddings_path=None):
+    texts = [doc.split() for doc in text_for_bow]
+
+    npmi_metric = CoherenceNPMI(topics, texts)
+    npmi_score = npmi_metric.score()
+    cv_metric = CoherenceCV(texts=texts, topics=topics)
+    cv_score = cv_metric.score()
+    we_metric = CoherenceWordEmbeddings(topics=topics)
+    we_score = we_metric.score()
+    irbo_metric = InvertedRBO(topics=topics)
+    irbo_score = irbo_metric.score()
+    td_metric = TopicDiversity(topics=topics)
+    td_score = td_metric.score(topk=10)
+    return npmi_score, cv_score, we_score, irbo_score, td_score
+
+
+
 def main(args):
     text_file = os.path.join('resources', '20news_unprep.txt')
     bow_file = os.path.join('resources', '20news_prep.txt')
@@ -40,11 +67,28 @@ def main(args):
 
     training_dataset = qt.fit(text_for_contextual=text_for_contextual, text_for_bow=text_for_bow)
 
-    ctm = ZeroShotTM(bow_size=len(qt.vocab), contextual_size=768, n_components=50)
+    for num_topics in [25, 50, 75, 100, 150]:
+        npmi_scores = []
+        cv_scores = []
+        we_scores = []
+        irbo_scores = []
+        td_scores = []
 
-    ctm.fit(training_dataset)
-    ctm.get_topics(2)
-    pdb.set_trace()
+        for seed in range(30):
+            set_random_seed(seed)
+            ctm = CombinedTM(bow_size=len(qt.vocab), contextual_size=768, n_components=num_topics)
+
+            ctm.fit(training_dataset)
+            topics = [v for k, v in ctm.get_topics(10).items()]
+            scores = evaluate(topics, text_for_bow, embeddings_path=None)
+
+            npmi_scores.append(scores[0])
+            cv_scores.append(scores[1])
+            we_scores.append(scores[2])
+            irbo_scores.append(scores[3])
+            td_scores.append(scores[4])
+
+        print(f"[{num_topics}-Topics] NPMI: {np.mean(npmi_scores)}, CV: {np.mean(npmi_scores)}, WE: {np.mean(we_scores)}, I-RBO: {np.mean(irbo_scores)}, TD: {np.mean(td_scores)}")
 
 
 
